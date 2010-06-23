6065df3f1d2e5ec445b6051f1e716ce6bbbd0e19
Mike Zupan <mzupan@theopenskyproject.com>
2010-06-23 14:49:38 -0400

    added in bytes total

--- get_varnish_stats.py
+++ get_varnish_stats.py
@@ -36,6 +36,8 @@ def main(argv):
 
 
 def get_stats(host, port):
+    stats = {}
+    
     telnet = telnetlib.Telnet()
     telnet.open(host, port)
     telnet.write('stats\r\n')
@@ -52,11 +54,32 @@ def get_stats(host, port):
     hits = re.search("\d+  Cache hits", out).group(0).split()[0]
     hits_pass = re.search("\d+  Cache hits for pass", out).group(0).split()[0]
     miss = re.search("\d+  Cache misses", out).group(0).split()[0]
-    hit_percent = round((float(hits) + float(hits_pass) / (float(hits) + float(miss) + float(hits_pass))) * 100, 1)
+    hit_percent = round(((float(hits) + float(hits_pass)) / (float(hits) + float(miss) + float(hits_pass))) * 100, 1)
     
+    stats['requests'] = req
+    stats['cache_hits'] = hits
+    stats['cache_hits_pass'] = hits_pass
+    stats['cache_miss'] = miss
+    stats['cache_percentage'] = str(hit_percent)
     
+    #
+    # pulling out the cache size usage
+    #
+    bytes_free = re.search("\d+  bytes allocated", out).group(0).split()[0]
+    bytes_used = re.search("\d+  bytes free", out).group(0).split()[0]
+    bytes_total = int(bytes_free) + int(bytes_used)
     
-    print "requests:"+ req +" cache_hits:"+ hits +" cache_hits_pass:"+ hits_pass +" cache_miss:"+ miss
+    stats['bytes_free'] = bytes_free
+    stats['bytes_used'] = bytes_used
+    stats['bytes_total'] = str(bytes_total)
+    
+    #
+    # iterate through the dictionary
+    #
+    out = ""
+    for k,v in stats.items():
+        out = "%s:%s %s" % (k,v,out)
 
+    print out
 if __name__ == "__main__":
     main(sys.argv[1:])
